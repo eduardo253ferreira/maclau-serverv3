0f925c4e7fd30e0522e526c98652efcb0483cd1f
eduardo253ferreira <eduardo253ferreira@gmail.com>
2026-06-17 17:47:22 +0100

    Delete scratch directory

--- scratch/view_admin_relatorio.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-const content = fs.readFileSync('public/js/admin.js', 'utf8');
-const lines = content.split('\n');
-
-for (let i = 3599; i < 3705; i++) {
-    if (lines[i] !== undefined) {
-        console.log(`${i + 1}: ${lines[i]}`);
-    }
-}
--- scratch/view_parse_parts.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-const content = fs.readFileSync('server.js', 'utf8');
-const lines = content.split('\n');
-
-for (let i = 3700; i < 3760; i++) {
-    if (lines[i] !== undefined) {
-        console.log(`${i + 1}: ${lines[i]}`);
-    }
-}
